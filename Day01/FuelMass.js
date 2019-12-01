574e89f463dde7ff84753c519b67503209afd0e3
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-01 22:23:25 +0000

    First day part 2

--- Day01/FuelMass.js
+++ Day01/FuelMass.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 
 var input = fs.readFileSync('input.txt').toString().split("\r\n");
-console.log(input);
 
 var array = [];
 for (i=0; i<input.length; i++){
-    array.push((Math.floor(input[i]/3)-2));
+    var firstMass = (Math.floor(input[i]/3))-2;
+    console.log("First is " + firstMass);
+    var totalMass = [firstMass];
+    while(firstMass > 5){
+        firstMass = (Math.floor(firstMass/3))-2;
+        totalMass.push(firstMass);
+    };
+    console.log(totalMass);
+    moduleMass = totalMass.reduce(addingFunction);
+
+    array.push(moduleMass);
 }
 console.log(array);
 
