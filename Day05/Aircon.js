d7b71167bf51ea47f65df8c1665d780fd8d5d1b5
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-06 14:08:32 +0000

    Day5 part 2 finished

--- Day05/Aircon.js
+++ Day05/Aircon.js
@@ -20,6 +20,10 @@ function readOpcode(memory){
             : opcode == 2 ? multiplyingFunction(param1, paramMode1, param2, paramMode2, param3, paramMode3)
             : opcode == 3 ? storageFunction(param1)
             : opcode == 4 ? outputtingFunction(param1)
+            : opcode == 5 ? jumpIfTrueFunction(param1, paramMode1, param2, paramMode2)
+            : opcode == 6 ? jumpIfFalseFunction(param1, paramMode1, param2, paramMode2)
+            : opcode == 7 ? lessThanFunction(param1, paramMode1, param2, paramMode2, param3)
+            : opcode == 8 ? equalsFunction(param1, paramMode1, param2, paramMode2, param3)
             : i = (memory.length +1);
     }
     return memory[0];
@@ -53,7 +57,7 @@ function multiplyingFunction(param1, paramMode1, param2, paramMode2, param3, par
 
 function storageFunction(param1){
     //do something to get an input, and save that to integerInput - for now just giving it 1
-    integerInput = 1;
+    integerInput = 5;
     a = parseInt(param1)
     memory.splice(a, 1, integerInput);
     i=i+2
@@ -63,4 +67,30 @@ function outputtingFunction(param1){
     output = parseInt(memory[param1])
     console.log("The output is " + output) 
     i=i+2
+}
+
+function jumpIfTrueFunction(param1, paramMode1, param2, paramMode2){
+    paramMode1 == 0 ? a = parseInt(memory[param1]) : a = parseInt(param1)
+    paramMode2 == 0 ? b = parseInt(memory[param2]) : b = parseInt(param2)
+    a != 0 ? i = b : i = i+3
+}
+
+function jumpIfFalseFunction(param1, paramMode1, param2, paramMode2){
+    paramMode1 == 0 ? a = parseInt(memory[param1]) : a = parseInt(param1)
+    paramMode2 == 0 ? b = parseInt(memory[param2]) : b = parseInt(param2)
+    a == 0 ? i = b : i = i+3
+}
+
+function lessThanFunction(param1, paramMode1, param2, paramMode2, param3){
+    paramMode1 == 0 ? a = parseInt(memory[param1]) : a = parseInt(param1)
+    paramMode2 == 0 ? b = parseInt(memory[param2]) : b = parseInt(param2)
+    a < b ? memory.splice(param3, 1, 1) : memory.splice(param3, 1, 0)
+    i=i+4;
+}
+
+function equalsFunction(param1, paramMode1, param2, paramMode2, param3){
+    paramMode1 == 0 ? a = parseInt(memory[param1]) : a = parseInt(param1)
+    paramMode2 == 0 ? b = parseInt(memory[param2]) : b = parseInt(param2)
+    a == b ? memory.splice(param3, 1, 1) : memory.splice(param3, 1, 0)
+    i=i+4;
 }
