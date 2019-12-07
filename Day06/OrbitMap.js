046373a22652b3c7f47272b0b2b36df887eabff7
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-07 18:23:48 +0000

    Day 6 part 2

--- Day06/OrbitMap.js
+++ Day06/OrbitMap.js
@@ -8,12 +8,51 @@ while(i<input.length){
     i++
 }
 
-allTheOrbitChainArrays = []
-for(a=0 ; a<input.length; a++){
-    orbited = input[a][0].toString()
+allTheOrbitChainArrays = allTheOrbits(input)
+totalOrbits = allTheOrbitChainArrays.reduce(addingFunction);
+console.log(totalOrbits)
+
+fullSantaChain = singleOrbitChain(input, "SAN")
+fullMyChain = singleOrbitChain(input, "YOU")
+totalHops = orbitChecker(fullSantaChain, fullMyChain)
+console.log(totalHops)
+
+function orbitChecker(santaChain, myChain){
+    for(a=0; a<myChain.length; a++){
+        for(b=0; b<santaChain.length; b++){
+            if(myChain[a] == santaChain[b]){
+                myHops = (a)
+                santaHops = (b)
+                return (myHops + santaHops)
+            }
+        }
+    }
+}
+
+function singleOrbitChain(input, name){
+    for(a=0 ; a<input.length; a++){
+        if(input[a][1] == name){
+            sanIndex = a
+        }
+    }
+    orbited = input[sanIndex][0].toString()
+    singleOrbitChainArray = createOrbitChainArray(input, orbited)
+    return singleOrbitChainArray
+}
+
+function allTheOrbits(input){
+    allTheOrbitChainArrays = []
+    for(a=0 ; a<input.length; a++){
+        orbited = input[a][0].toString()
+        orbitChainArray = createOrbitChainArray(input, orbited)
+        allTheOrbitChainArrays.push(orbitChainArray.length)
+    }
+    return allTheOrbitChainArrays
+}
+
+function createOrbitChainArray(input, orbited, orbitChainArray){
     orbitChainArray = []
     orbitChainArray.push(orbited)
-
     b=0;
     while(b<input.length){
         orbiter = input[b][1].toString()
@@ -24,13 +63,11 @@ for(a=0 ; a<input.length; a++){
         }
         b++
     }
-    allTheOrbitChainArrays.push(orbitChainArray.length)
+    return orbitChainArray
 }
-
-totalOrbits = allTheOrbitChainArrays.reduce(addingFunction);
 
 function addingFunction(total, value){
     return total + value;
 }
 
-console.log(totalOrbits)
+
