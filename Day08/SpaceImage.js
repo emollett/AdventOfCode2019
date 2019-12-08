0671cad0ab36caf121407caa13d7ac62058505b8
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-08 14:11:03 +0000

    Day 8 Part 1

---
+++ Day08/SpaceImage.js
@@ -0,0 +1,45 @@
+fs = require('fs');
+
+input = fs.readFileSync('input.txt').toString().split("");
+
+imageWidth = 25
+imageHeight = 6
+pixelsInLayer = imageWidth*imageHeight
+
+layerArray = []
+for(i=0; i<input.length; i++){
+    layer = []
+    for(a=0; a<pixelsInLayer; a++){
+        pixel = input.pop()
+        layer.push(pixel)
+    }
+    layerArray.push(layer)
+}
+
+zeroArray = []
+oneArray = []
+twoArray = []
+for(i=0; i<layerArray.length; i++){
+    zero = 0
+    one = 0
+    two = 0
+    for(a=0; a<pixelsInLayer; a++){
+        layerArray[i][a] == 0 ? zero ++
+        : layerArray[i][a] == 1 ? one ++
+        : layerArray[i][a] == 2 ? two ++ 
+        : console.log("none here")
+    }
+    zeroArray.push(zero)
+    oneArray.push(one)
+    twoArray.push(two)
+}
+
+indexOfLeastZeros = indexOfSmallest(zeroArray)
+console.log(indexOfLeastZeros)
+console.log("The number of ones in the layer with least zeros is " + oneArray[indexOfLeastZeros])
+console.log("The number of twos in the layer with least zeros is " + twoArray[indexOfLeastZeros])
+console.log("QA answer " + (oneArray[indexOfLeastZeros] * twoArray[indexOfLeastZeros]))
+
+function indexOfSmallest(a) {
+    return a.indexOf(Math.min.apply(Math, a));
+   }
