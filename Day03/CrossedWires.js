328ab06011a7a434e73927ac7d8b10f5b4c20adb
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-10 18:09:52 +0000

    Day 3 part 2 still not finished

--- Day03/CrossedWires.js
+++ Day03/CrossedWires.js
@@ -1,6 +1,6 @@
 fs = require('fs');
 
-input = fs.readFileSync('testInput.txt').toString().split("\r\n");
+input = fs.readFileSync('input.txt').toString().split("\r\n");
 
 input.splice(0, 1, input[0].toString().split(","));
 input.splice(1, 1, input[1].toString().split(","));
@@ -8,21 +8,22 @@ input.splice(1, 1, input[1].toString().split(","));
 wireVectorsA = vectorDirections(input[0]);
 wireVectorsB = vectorDirections(input[1]);
 
+// console.log(wireVectorsA)
+
 coordArrayA = allTheCoordinates(wireVectorsA);
 coordArrayB = allTheCoordinates(wireVectorsB);
 // console.log(coordArrayA);
 // console.log(coordArrayB);
 
-matchingValues = compareArrays(coordArrayA, coordArrayB);
-console.log(matchingValues);
+matchingValuesA = compareArrays(coordArrayA, coordArrayB);
+matchingValuesB = compareArrays(coordArrayB, coordArrayA);
+console.log(matchingValuesA)
 
-smallestDistance = smallestDistance(matchingValues)
+smallestDistance = smallestDistance(matchingValuesA)
 console.log("The distances where they intersect are " + smallestDistance)
 
-numberOfSteps= numberOfSteps(matchingValues, coordArrayA, coordArrayB)
-console.log(numberOfSteps)
-
-//use the values returned for the intersections (matching values) and test them against the full coordArray for each to get the index then add together
+// numberOfSteps= numberOfSteps(matchingValuesA, matchingValuesB, coordArrayA, coordArrayB)
+// console.log("The number of steps is " + numberOfSteps)
 
 function vectorDirections(wire){
     wireVectors = [[0,0]]
@@ -38,7 +39,7 @@ function vectorDirections(wire){
 }
 
 function allTheCoordinates(wireVectors){
-    coordArray = [[0,0]]
+    coordArray = []
     i=0;
     coordA = 0;
     coordB = 0;
@@ -99,7 +100,7 @@ function allTheCoordinates(wireVectors){
             }
         }
         else if(a==0 && b==0){
-            console.log("all 0");
+            // console.log("all 0");
         }
         else if(a<0 && b==0){
             while(aa>a){
@@ -137,7 +138,7 @@ function compareArrays(coordArrayA, coordArrayB){
     while(x<=coordArrayB.length){
         var coordB = coordArrayB[x];
         if(coordObjectA.hasOwnProperty(coordB)) {
-            matchingValues.push(coordArrayB[x]);
+            matchingValues.push([coordArrayB[x][0], coordArrayB[x][1], x]);
         }
         x++
     }
@@ -158,39 +159,17 @@ function smallestDistance(matchingValues){
     return smallestDistance;
 }
 
-function numberOfSteps(matchingValues, coordArrayA, coordArrayB){
-    var matchingValuesObject = {};
-    for(var i = 0 ; i < matchingValues.length; i += 1) {
-        matchingValuesObject[matchingValues[i]] = i;
-    };
-    console.log(matchingValuesObject)
+function numberOfSteps(matchingValuesA, matchingValuesB){
 
-    stepsA = matchCrossPoints(coordArrayA, matchingValuesObject);
-    stepsB = matchCrossPoints(coordArrayB, matchingValuesObject);
-    console.log(stepsA)
-    console.log(stepsB)
+
+
 
     totalNumberOfSteps = []
     z=0
-    while(z<stepsA.length){
-        totalNumberOfSteps.push(stepsA[z]+stepsB[z]);
+    while(z<matchingValues.length){
+        totalNumberOfSteps.push(matchingValues[z][2]+matchingValues[z][3]);
         z++
     }
     totalNumberOfSteps.sort(function(a, b){return a-b});
     return(totalNumberOfSteps)
 }
-
-function matchCrossPoints(coordArray, matchingValuesObject){
-    steps=[];
-    x=0
-    while(x<coordArray.length){
-        var coord = coordArray[x];
-        if(matchingValuesObject.hasOwnProperty(coord)) {
-            steps.push([coordArray.indexOf(coord)-1, coord]);
-        }
-        x++
-    };
-    return steps  
-}
-
-//next use the coordinates from each steps array to match up the steps to add
