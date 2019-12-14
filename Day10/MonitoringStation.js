cad856568649c084bb67f8fa024970b578a262c7
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-12-14 13:12:09 +0000

    Day10 part 1

--- Day10/MonitoringStation.js
+++ Day10/MonitoringStation.js
@@ -1,6 +1,6 @@
 fs = require('fs');
 
-input = fs.readFileSync('testInput.txt').toString().split("\r\n");
+input = fs.readFileSync('input.txt').toString().split("\r\n");
 
 i=0
 while(i<input.length){
@@ -32,10 +32,10 @@ console.log(asteroidCoordinates[index])
 
 function detectAsteroids(){
     numberDetected = []
-    // for(e=0; e<asteroidCoordinates.length; e++){
-        monitoringAsteroid = asteroidCoordinates.slice(7,8)
+    for(e=0; e<asteroidCoordinates.length; e++){
+        monitoringAsteroid = asteroidCoordinates.slice(e,e+1)
         tempAsteroidCoordinates = [...asteroidCoordinates]
-        tempAsteroidCoordinates.splice(7,1)
+        tempAsteroidCoordinates.splice(e,1)
         // console.log(monitoringAsteroid)
         // console.log(tempAsteroidCoordinates)
     
@@ -45,18 +45,18 @@ function detectAsteroids(){
             // console.log(Number(monitoringAsteroid[0][0]))
             directions.push([tempAsteroidCoordinates[c][0]-monitoringAsteroid[0][0], tempAsteroidCoordinates[c][1]-monitoringAsteroid[0][1]])
         }
-        console.log(directions)
+        // console.log(directions)
         values = []
         for(d=0; d<directions.length; d++){
             //current issue is here -> -1/-2 is the same as 1/2 - need to give directionality
-            values.push(directions[d][0]/directions[d][1])
+            values.push(Math.atan2(directions[d][1], directions[d][0]))
         }
-        console.log(values)
+        // console.log(values)
         //there is another issue here, with sets -0 is the same as 0, I need them not to be
         dedupedValues = Array.from(new Set(values))
         // console.log(dedupedValues)
         numberDetected.push(dedupedValues.length)
-    // }
+    }
     return numberDetected
 
 }
